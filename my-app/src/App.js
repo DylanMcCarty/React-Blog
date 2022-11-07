0ea0c9a78940864170691034957513f4a066949d
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-11-07 17:57:19 +0000

    added a hero that adds scroll functionality to scroll down and see more

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Hero'
+import AboutMe from './AboutMe'
+
+export default function App() {
+  return (
+    <div>
+        <Header />
+        <AboutMe />
+    </div>
+  )
+}
